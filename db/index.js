6a85431e0db955209afb980e8b05d643365fb6e3
cachecleanerjeet <51857187+cachecleanerjeet@users.noreply.github.com>
2021-09-05 15:09:21 +0530

    db enviroment change

--- db/index.js
+++ db/index.js
@@ -2,10 +2,13 @@ const { MongoClient } = require("mongodb");
 require("dotenv").config();
 
 module.exports = async (database) => {
-  var conn = await MongoClient.connect(process.env.MONGODB_URL, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  });
+  var conn = await MongoClient.connect(
+    process.env.MONGO_URL || process.env.MONGODB_URL,
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    }
+  );
   return {
     conn,
     db: conn.db(database || process.env.DATABASE_NAME),
